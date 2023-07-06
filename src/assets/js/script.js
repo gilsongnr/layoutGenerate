896da250f5f03e4d661d4e0fd2520e4ed40c2e69
gilsongnr <gilson.gnr@gmail.com>
2023-07-06 15:33:11 -0300

    Add files via upload

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,13 +6,26 @@ const labelfilename = document.getElementById("labelfilename")
 edtFileName.onchange = ()=>{
   labelfilename.innerHTML = edtFileName.value
 }
-
+function createDiv(){
+    return document.createElement("div")
+}
+function createDivClass(classList){
+    let div = createDiv()
+    div.classList = classList
+    return div
+}
+function createDivClasses(classeList){
+  let div = createDiv()
+  classeList.forEach(s => {
+    div.classList.add(s)
+  });
+  return div
+}
 function createDivCtrl(classList, input, title){
       if (title == undefined){
         title = input.placeholder
       }
-      let div = document.createElement("div")
-      div.classList = classList
+      let div = createDivClass(classList)
       let label = div.appendChild(document.createElement("label"))
       label.setAttribute("for", input.id)
       label.innerHTML = title
@@ -77,8 +90,7 @@ function createLine(lineCode, show){
           const collapseCtrlID = "collapse-" + this.id
           const collapseClass = "collapse"
 
-          this.line = document.createElement("div")   
-          this.line.classList = "accordion-item"
+          this.line = createDivClass("accordion-item")
               
           let ctrl = this.line.appendChild(document.createElement("h2"))
           ctrl.classList = "accordion-header"
@@ -94,25 +106,19 @@ function createLine(lineCode, show){
           this.btnheader.innerHTML = "Linha: " + lineCode
           this.btnheader.obj = this
 
-          const collapseCtrl = this.line.appendChild(document.createElement("div"))
+          const collapseCtrl = this.line.appendChild(createDivClasses(["accordion-collapse", collapseClass]))
           collapseCtrl.id = collapseCtrlID
-          collapseCtrl.classList = "accordion-collapse"
-          collapseCtrl.classList.add(collapseClass)
           if (show){
             collapseCtrl.classList.add("show")
           }
           collapseCtrl.setAttribute("aria-labelledby", lineID)
           collapseCtrl.setAttribute("data-bs-parent", "#accordionFile")
-          const dbody = collapseCtrl.appendChild(document.createElement("div"))
-          dbody.classList = "accordion-body"
-          let dbodyA = dbody.appendChild(document.createElement("div"))
-          dbodyA.classList = "row"    
-
-          ctrl = dbodyA.appendChild(document.createElement("div"))
-          ctrl.classList = "col"
+          const dbody = collapseCtrl.appendChild(createDivClass("accordion-body"))
+          let dbodyA = dbody.appendChild(createDivClass("row"))
+          let col = dbodyA.appendChild(createDivClass("col"))
 
           this.line_header = createInput("line_header" + this.id, "", false, "checkbox", "form-check-input")                    
-          ctrl.appendChild(createDivCtrl("form-check", this.line_header, "Essa linha é cabeçalho"))
+          col.appendChild(createDivCtrl("form-check", this.line_header, "Essa linha é cabeçalho"))
           
           this.line_header.onclick = ()=>{
             if (this.line_header.checked){
@@ -126,14 +132,13 @@ function createLine(lineCode, show){
             }
             this.doSetIsHeader()
           }
-          let divBtns = dbodyA.appendChild(document.createElement("div"))
-          divBtns.classList = "col-auto"
-          const btnAddCol = divBtns.appendChild(document.createElement("btn"))
+          col = dbodyA.appendChild(createDivClass("col-auto"))
+          const btnAddCol = col.appendChild(document.createElement("button"))
           btnAddCol.classList = "btn btn-info me-1"
           btnAddCol.innerHTML = "Add Coluna"
           btnAddCol.type = "button"
 
-          ctrl = divBtns.appendChild(document.createElement("btn"))
+          ctrl = col.appendChild(document.createElement("button"))
           ctrl.classList = "btn btn-danger"
           ctrl.innerHTML = "Excluir linha"
           ctrl.type = "button"
@@ -143,8 +148,7 @@ function createLine(lineCode, show){
             }
           }
           
-          dbodyA = dbody.appendChild(document.createElement("div"))
-          dbodyA.classList = "row"    
+          dbodyA = dbody.appendChild(createDivClass("row"))
           this.line_level = createInputNumber("line_level" + this.id)
           dbodyA.appendChild(createDivGroup("col-4 col-sm-3 col-md-3", this.line_level, "Nível"))
        
@@ -161,8 +165,7 @@ function createLine(lineCode, show){
           this.line_descr.id = "line_descr" + this.id
           dbodyA.appendChild(createDivGroup("col-12", this.line_descr, "Descrição da Linha"))
          
-          let div = dbodyA.appendChild(document.createElement("div"))
-          div.classList = "table-container text-center"
+          let div = dbodyA.appendChild(createDivClass("table-container text-center"))
           let h5 = div.appendChild(document.createElement("h5"))
           h5.innerHTML = "Colunas"
           this.tableCols = div.appendChild(document.createElement("table"))
@@ -192,31 +195,43 @@ const iconDelete =
 // `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105.16 122.88"><defs><style>.cls-1{fill-rule:evenodd;}</style></defs><title>delete</title><path class="cls-1" d="M11.17,37.16H94.65a8.4,8.4,0,0,1,2,.16,5.93,5.93,0,0,1,2.88,1.56,5.43,5.43,0,0,1,1.64,3.34,7.65,7.65,0,0,1-.06,1.44L94,117.31v0l0,.13,0,.28v0a7.06,7.06,0,0,1-.2.9v0l0,.06v0a5.89,5.89,0,0,1-5.47,4.07H17.32a6.17,6.17,0,0,1-1.25-.19,6.17,6.17,0,0,1-1.16-.48h0a6.18,6.18,0,0,1-3.08-4.88l-7-73.49a7.69,7.69,0,0,1-.06-1.66,5.37,5.37,0,0,1,1.63-3.29,6,6,0,0,1,3-1.58,8.94,8.94,0,0,1,1.79-.13ZM5.65,8.8H37.12V6h0a2.44,2.44,0,0,1,0-.27,6,6,0,0,1,1.76-4h0A6,6,0,0,1,43.09,0H62.46l.3,0a6,6,0,0,1,5.7,6V6h0V8.8h32l.39,0a4.7,4.7,0,0,1,4.31,4.43c0,.18,0,.32,0,.5v9.86a2.59,2.59,0,0,1-2.59,2.59H2.59A2.59,2.59,0,0,1,0,23.62V13.53H0a1.56,1.56,0,0,1,0-.31v0A4.72,4.72,0,0,1,3.88,8.88,10.4,10.4,0,0,1,5.65,8.8Zm42.1,52.7a4.77,4.77,0,0,1,9.49,0v37a4.77,4.77,0,0,1-9.49,0v-37Zm23.73-.2a4.58,4.58,0,0,1,5-4.06,4.47,4.47,0,0,1,4.51,4.46l-2,37a4.57,4.57,0,0,1-5,4.06,4.47,4.47,0,0,1-4.51-4.46l2-37ZM25,61.7a4.46,4.46,0,0,1,4.5-4.46,4.58,4.58,0,0,1,5,4.06l2,37a4.47,4.47,0,0,1-4.51,4.46,4.57,4.57,0,0,1-5-4.06l-2-37Z"/></svg>
 // `
 function createButtonDelete(){
-  const div = document.createElement("div")
+  const div = createDiv()
   div.innerHTML = iconDelete
   div.style.cursor = "pointer"
   return div
 }
+function createInputTextArea(){
+  const div = document.createElement("textarea")
+  return div
+}
+
 function tableColsAddRow(table,  name, tp, pos, size, dec, lit, check, desc){
   let row = table.insertRow();
-  function add(value){
+  const get1 = (s, cel)=>{cel.textContent = s}
+  const get2 = (s, cel)=>{cel.innerHTML = s.replaceAll("\n", "<br>")}
+  function add(value, proc){
     let cell = row.insertCell();
     if (value != undefined) {
-      if (value != ""){
-        value = value.replaceAll("\n", "<br>")
-      }
-      cell.innerHTML = value;
+      proc(value, cell)
     }
     return cell
   }
-  add(name).style.textAlign = "left"
-  add(tp) 
-  add(pos) 
-  add(size) 
-  add(dec) 
-  add(lit) 
-  add(check).style.textAlign = "left" 
-  add(desc).style.textAlign = "justify"
+  function add1(value){
+    return add(value, get1)
+  }
+  function add2(value){
+    return add(value, get2)
+  }
+  add1(name).style.textAlign = "left"
+  add1(tp) 
+  add1(pos) 
+  add1(size) 
+  add1(dec) 
+  add1(lit) 
+  //add().appendChild(createInputTextArea()).value = check 
+  //add().appendChild(createInputTextArea()).value = desc
+  add2(check).style.textAlign = "left"
+  add2(desc).style.textAlign = "justify"
   //add("x")
   row.insertCell().appendChild(createButtonDelete()).onclick = ()=>{
     if (confirm("Excluir coluna: " + row.cells[0].textContent)){
@@ -261,27 +276,44 @@ function getCols(obj){
   const cols = []
 
   for (let i = 1; i < rows.length; i++) {
+    const colValues = []
+    function add(cell){
+      colValues.push(cell.textContent)//innerHTML.replaceAll("<br>", "\n"))
+    }
     const r = rows[i].cells;
-    let colValues = []
-     for (let x = 0; x < 7; x++) {
-       colValues.push(r[x].textContent)
+     for (let x = 0; x < 6; x++) {
+       add(r[x])
      }
+     //console.log(r[0].textContent)
+     //let txt = r[6].querySelector("textarea")//-- r[7].getElementById 
+     //console.log(txt)
+     //console.log(txt.value)
+     //console.log(r[6].childNodes[0])
+     //add(txt.value)
+     //add()
+     colValues.push(r[6].innerHTML.replaceAll("<br>", "\n"))
      for (let x = 7; x < 11; x++) {
          colValues.push("")
      }
-    colValues.push(r[7].textContent)
-    cols.push(colValues)    
+     //txt = r[7].querySelector("textarea")//-- r[7].getElementById 
+     //console.log("r[7]")
+     //console.log(txt)
+     //console.log(txt.value)
+     //add(txt.value)
+     colValues.push(r[7].innerHTML.replaceAll("<br>", "\n"))
+     cols.push(colValues)    
   } 
   return cols
 }
 function getLine(obj){
-  return {values: [obj.code, "", obj.line_level.value, "", "", obj.line_parente.value, "", "", "", "", obj.table_sulfix.value, obj.line_descr.value], cols: getCols(obj)};  
+  return {values: [obj.code, "", obj.line_level.value, "", "", obj.line_parente.value, 
+                    "", "", "", "", obj.table_sulfix.value, obj.line_descr.value], cols: getCols(obj)};  
 }
 function getLines(){
-  let aa = accordionFile.querySelectorAll('.accordion-button.line-header');
+  let a = accordionFile.querySelectorAll('.accordion-button.line-header');
   let lines = []
-    for (let i = 0; i < aa.length; i++) {
-        lines.push(getLine(aa[i].obj))
+    for (let i = 0; i < a.length; i++) {
+        lines.push(getLine(a[i].obj))
     }   
     return lines
 }
@@ -320,8 +352,8 @@ function saveToFile(){
   
   lines.forEach(line => {
     csvContent += "L" + joinList(line.values) + "\n"
-    line.cols.forEach(line => {
-      csvContent += "C" + joinList(line) + "\n"
+    line.cols.forEach(col => {
+      csvContent += "C" + joinList(col) + "\n"
     })
   });
   
