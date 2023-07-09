acc609cb1dc602b4670981f5a1c9177f2df41cd8
gilsongnr <gilson.gnr@gmail.com>
2023-07-08 23:29:23 -0300

    Add files via upload

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,6 +6,119 @@ const labelfilename = document.getElementById("labelfilename")
 edtFileName.onchange = ()=>{
   labelfilename.innerHTML = edtFileName.value
 }
+
+const colNames = {
+    name: "name",
+    tp  : "tp",
+    pos: "pos", 
+    size: "size", 
+    dec: "dec", 
+    literal: "literal", 
+    check: "Check", 
+    descr: "descr",
+    list: ["name", "tp", "pos", "size", "dec", "literal", "Check", "descr"],
+    titles: ["Nome", "Tipo", "Posição", "Tamanho", "Decimais", "Valor literal", "Check", "Descrição", ""]
+}
+const classCtrlError = "is-invalid"
+function checkText(s){
+      return (s != undefined) && (s.trim() != "")
+}
+const columnEditor = {
+  form   : new bootstrap.Modal(document.getElementById('edt_colModal'), {}),
+  getCtrl: function (colName){return document.getElementById("edt_col_" + colName)},
+  setCtrlNoError: function(ctrl){ctrl.classList.remove(classCtrlError)},
+  setCtrlValueByName: function(name, value){
+                          const edt = this.getCtrl(name)
+                          edt.value = value
+                          this.setCtrlNoError(edt)                
+                      },
+  validateCtrls: function(){
+                          let erros = 0;
+                          const obj = this
+                          function setError(edt){
+                              erros++;
+                              const evtName = "change" 
+                              //this.setCtrlInError(edt)
+                              edt.classList.add(classCtrlError)
+                              edt.addEventListener(evtName, modifyText);
+                              function modifyText(){
+                                  obj.setCtrlNoError(edt)
+                                  console.log("modifyText")
+                                  console.log(edt.id)
+                                  edt.removeEventListener(evtName, modifyText)
+                              }
+                          }
+                          function check(edt){
+                                        if (checkText(edt.value)) {
+                                          return true
+                                        }
+                                        setError(edt)
+                                        return false  
+                                    }
+                          
+                          if ((this.edtCtrlName.value == undefined) || (this.edtCtrlName.value == "") || (!this.edtCtrlName.value.match(/^[A-Za-z_]+[\w_]*$/))){
+                                setError(this.edtCtrlName)
+                          }
+                          
+                          if (check(this.edtCtrlTp)){
+                              if (this.edtCtrlTp.value == "E")
+                              check(this.getCtrl(colNames.check))
+                          }
+                          return erros
+                    },
+  formExecute: function(then){
+                          const obj = this
+                          function saveClick(evt){
+                              if (obj.validateCtrls() == 0){
+                                    then()
+                                    obj.form.hide()                              
+                              }
+                          }
+                          this.getCtrl("btn_save").onclick = saveClick
+                          this.form.show()
+                    },                                  
+  addRow : function(table){    
+                  for (let i = 0; i < 8; i++) {this.setCtrlValueByName(colNames.list[i], "")} 
+                  if (table.rows.length > 1){
+                      const lastRow = table.rows[table.rows.length - 1]
+                      this.getCtrl(colNames.pos).value = parseInt(lastRow.cells[2].textContent) + parseInt(lastRow.cells[3].textContent)
+                  }
+                  this.formExecute(()=>{
+                      tableColsAddRow(table, this.edtCtrlName.value, this.edtCtrlTp.value, this.getCtrl(colNames.pos).value, this.getCtrl(colNames.size).value, 
+                                              this.getCtrl(colNames.dec).value, this.getCtrl(colNames.literal).value, this.getCtrl(colNames.check).value, this.getCtrl(colNames.dec).value)
+                       
+                  })
+              },
+  edit: function(cells){  
+                  for (let i = 0; i < 6; i++) {this.setCtrlValueByName(colNames.list[i], getCellA(cells[i]))}                 
+                  this.setCtrlValueByName("Check", getCellB(cells[6]))
+                  this.setCtrlValueByName("descr", getCellB(cells[7]))
+                  this.formExecute(()=>{ 
+                      for (let i = 0; i < 6; i++) {
+                        setCellA(this.getCtrl(colNames.list[i]).value, cells[i])
+                      }   
+                      setCellB(this.getCtrl("Check").value, cells[6])
+                      setCellB(this.getCtrl("descr").value, cells[7])
+                  })
+        },
+  init: function(){
+        this.edtCtrlName = this.getCtrl("name")
+        this.edtCtrlTp = this.getCtrl("tp")
+        this.edtCtrlDec = this.getCtrl("dec")        
+        this.edtCtrlTp.onchange = ()=>{
+          this.edtCtrlDec.disabled = this.edtCtrlTp.value != "N"
+        }
+      }
+
+}
+columnEditor.init()
+
+const setCellA = (s, cel)=>{cel.textContent = s}
+const setCellB = (s, cel)=>{cel.innerHTML = s.replaceAll("\n", "<br>")}
+
+const getCellA = (cel)=>{return cel.textContent}
+const getCellB = (cel)=>{return cel.innerHTML.replaceAll("<br>", "\n")}
+
 function createDiv(){
     return document.createElement("div")
 }
@@ -55,19 +168,12 @@ function createInput(id, placeholder, required, inputType, classList){
       }
       return input
 } 
-function createInputText(id, placeholder, required){
-      return createInput(id, placeholder, required, "text")
-} 
-function createInputNumber(id, placeholder, required){
-      return createInput(id, placeholder, required, "number")
-} 
-
-
+function createInputText(id, placeholder, required){return createInput(id, placeholder, required, "text")} 
+function createInputNumber(id, placeholder, required){return createInput(id, placeholder, required, "number")} 
 
 const accordionFile = document.getElementById("accordionFile");
 const collapseFile = document.getElementById("collapseFile")
 let lineCount = 0
-const colNames = ["Nome", "Tipo", "Posição", "Tamanho", "Decimais", "Valor literal", "Check", "Descrição"]
 
 function createLine(lineCode, show){ 
     const obj = {
@@ -102,7 +208,6 @@ function createLine(lineCode, show){
           this.btnheader.setAttribute("data-bs-target", "#"+collapseCtrlID)
           this.btnheader.setAttribute("aria-expanded", "false")
           this.btnheader.setAttribute("aria-controls", collapseCtrlID)
-          //this.btnheader.setAttribute("contenteditable", "true")
           this.btnheader.innerHTML = "Linha: " + lineCode
           this.btnheader.obj = this
 
@@ -169,76 +274,54 @@ function createLine(lineCode, show){
           let h5 = div.appendChild(document.createElement("h5"))
           h5.innerHTML = "Colunas"
           this.tableCols = div.appendChild(document.createElement("table"))
-          this.tableCols.classList = "table table-success table-striped table-bordered"
+          this.tableCols.classList = "table table-striped table-bordered"
           let tr = this.tableCols.appendChild(document.createElement('tr'));
-
-          colNames.forEach(i => {
-              tr.appendChild(document.createElement("th")).innerHTML = i            
-          });
-        
-          btnAddCol.onclick = ()=>{
-            tableColsAddRow(this.tableCols, "Col_" + this.tableCols.rows.length, "A", this.tableCols.rows.length + 1, "", "", "", "Coluna xx")                      
-          }
+          colNames.titles.forEach(i => {tr.appendChild(document.createElement("th")).innerHTML = i});        
+          btnAddCol.onclick = ()=>{columnEditor.addRow(this.tableCols)}
         }
     }
     obj.init();   
     return obj;
 }
 
-const iconDelete = 
-`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>
-`
-
-// `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105.16 122.88"><defs><style>.cls-1{fill-rule:evenodd;}</style></defs><title>delete</title><path class="cls-1" d="M11.17,37.16H94.65a8.4,8.4,0,0,1,2,.16,5.93,5.93,0,0,1,2.88,1.56,5.43,5.43,0,0,1,1.64,3.34,7.65,7.65,0,0,1-.06,1.44L94,117.31v0l0,.13,0,.28v0a7.06,7.06,0,0,1-.2.9v0l0,.06v0a5.89,5.89,0,0,1-5.47,4.07H17.32a6.17,6.17,0,0,1-1.25-.19,6.17,6.17,0,0,1-1.16-.48h0a6.18,6.18,0,0,1-3.08-4.88l-7-73.49a7.69,7.69,0,0,1-.06-1.66,5.37,5.37,0,0,1,1.63-3.29,6,6,0,0,1,3-1.58,8.94,8.94,0,0,1,1.79-.13ZM5.65,8.8H37.12V6h0a2.44,2.44,0,0,1,0-.27,6,6,0,0,1,1.76-4h0A6,6,0,0,1,43.09,0H62.46l.3,0a6,6,0,0,1,5.7,6V6h0V8.8h32l.39,0a4.7,4.7,0,0,1,4.31,4.43c0,.18,0,.32,0,.5v9.86a2.59,2.59,0,0,1-2.59,2.59H2.59A2.59,2.59,0,0,1,0,23.62V13.53H0a1.56,1.56,0,0,1,0-.31v0A4.72,4.72,0,0,1,3.88,8.88,10.4,10.4,0,0,1,5.65,8.8Zm42.1,52.7a4.77,4.77,0,0,1,9.49,0v37a4.77,4.77,0,0,1-9.49,0v-37Zm23.73-.2a4.58,4.58,0,0,1,5-4.06,4.47,4.47,0,0,1,4.51,4.46l-2,37a4.57,4.57,0,0,1-5,4.06,4.47,4.47,0,0,1-4.51-4.46l2-37ZM25,61.7a4.46,4.46,0,0,1,4.5-4.46,4.58,4.58,0,0,1,5,4.06l2,37a4.47,4.47,0,0,1-4.51,4.46,4.57,4.57,0,0,1-5-4.06l-2-37Z"/></svg>
-// `
-function createButtonDelete(){
-  const div = createDiv()
-  div.innerHTML = iconDelete
-  div.style.cursor = "pointer"
-  return div
-}
-function createInputTextArea(){
-  const div = document.createElement("textarea")
-  return div
+function createButtonSvg(svgClass){
+  const i = document.createElement("i")
+  i.classList = svgClass
+  i.style.fontSize = "1rem"
+  i.style.color = "#0d6efd"
+  return i
 }
 
 function tableColsAddRow(table,  name, tp, pos, size, dec, lit, check, desc){
-  let row = table.insertRow();
-  const get1 = (s, cel)=>{cel.textContent = s}
-  const get2 = (s, cel)=>{cel.innerHTML = s.replaceAll("\n", "<br>")}
+  let row = table.insertRow()
   function add(value, proc){
-    let cell = row.insertCell();
-    if (value != undefined) {
-      proc(value, cell)
-    }
-    return cell
+      let cell = row.insertCell()
+      if (value != undefined) {
+        proc(value, cell)
+      }
+      return cell
   }
-  function add1(value){
-    return add(value, get1)
-  }
-  function add2(value){
-    return add(value, get2)
-  }
-  add1(name).style.textAlign = "left"
-  add1(tp) 
-  add1(pos) 
-  add1(size) 
-  add1(dec) 
-  add1(lit) 
-  //add().appendChild(createInputTextArea()).value = check 
-  //add().appendChild(createInputTextArea()).value = desc
-  add2(check).style.textAlign = "left"
-  add2(desc).style.textAlign = "justify"
-  //add("x")
-  row.insertCell().appendChild(createButtonDelete()).onclick = ()=>{
+  function addA(value){return add(value, setCellA)}
+  let colName = addA(name)
+  colName.style.textAlign = "left"
+  //col.style.color = "#0d6efd"
+  colName.style.cursor = "pointer"
+  colName.classList.add("hover-green")
+  colName.onclick =  ()=>{columnEditor.edit(row.cells)}  
+
+  addA(tp) 
+  addA(pos) 
+  addA(size) 
+  addA(dec) 
+  addA(lit) 
+  add(check, setCellB).style.textAlign = "left"
+  add(desc, setCellB).style.textAlign = "justify"
+  row.insertCell().appendChild(createButtonSvg("bi bi-pencil-square hover-green")).onclick = colName.onclick
+  row.insertCell().appendChild(createButtonSvg("bi bi-trash3 hover-red")).onclick = ()=>{
     if (confirm("Excluir coluna: " + row.cells[0].textContent)){
       table.deleteRow(row.rowIndex);
     }
   }
-  //row.setAttribute("contenteditable", "true")
   return row;
 }
 
@@ -278,29 +361,17 @@ function getCols(obj){
   for (let i = 1; i < rows.length; i++) {
     const colValues = []
     function add(cell){
-      colValues.push(cell.textContent)//innerHTML.replaceAll("<br>", "\n"))
+      colValues.push(cell.textContent)
     }
     const r = rows[i].cells;
      for (let x = 0; x < 6; x++) {
        add(r[x])
      }
-     //console.log(r[0].textContent)
-     //let txt = r[6].querySelector("textarea")//-- r[7].getElementById 
-     //console.log(txt)
-     //console.log(txt.value)
-     //console.log(r[6].childNodes[0])
-     //add(txt.value)
-     //add()
-     colValues.push(r[6].innerHTML.replaceAll("<br>", "\n"))
+     colValues.push(getCellB(r[6]))
      for (let x = 7; x < 11; x++) {
          colValues.push("")
      }
-     //txt = r[7].querySelector("textarea")//-- r[7].getElementById 
-     //console.log("r[7]")
-     //console.log(txt)
-     //console.log(txt.value)
-     //add(txt.value)
-     colValues.push(r[7].innerHTML.replaceAll("<br>", "\n"))
+     colValues.push(getCellB(r[7]))
      cols.push(colValues)    
   } 
   return cols
